d15b10bb23160a0072591d286060e36d55c9df4d
Alexander Peredelskikh <be3predel@gmail.com>
2025-03-18 23:00:22 +0300

    update mainpage img animals

--- client/src/components/pages/MainPage/MainPage.tsx
+++ client/src/components/pages/MainPage/MainPage.tsx
@@ -36,11 +36,11 @@ const MainPage: React.FC = () => {
   const lastFourGadgets = gadgets.slice(-4); 
 
   const handleAllEventsClick = () => {
-    navigate('/events'); 
+    navigate('/event'); 
   };
 
   const handleAllGadgetsClick = () => {
-    navigate('/gadgets'); 
+    navigate('/gadget'); 
   };
 
   return (
