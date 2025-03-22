67a2d1c7a145ed489f6c59fb968d60f37cee0ec2
Lyubov Kabanova <lyubovrausch@gmail.com>
2025-03-22 13:27:50 +0300

    comment

--- client/src/redux/slice/gadget.api.slice.ts
+++ client/src/redux/slice/gadget.api.slice.ts
@@ -1,35 +1,37 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 interface GadgetResponse {
-	id?: number;
-	name: string;
-	user_id: number;
-	price: number;
+  id?: number;
+  name: string;
+  user_id: number;
+  price: number;
+  image?: string;
 }
 
 export const apiGadget = createApi({
-	reducerPath: "gadgetApi",
-	baseQuery: fetchBaseQuery({
-		baseUrl: `${import.meta.env.VITE_URL}/gadget`,
-		credentials: "include",
-	}),
-	tagTypes: ["Gadget"],
-	endpoints: builder => ({
-		createGadget: builder.mutation<
-			GadgetResponse,
-			{
-				id?: number;
-				name: string;
-				user_id: number;
-			}
-		>({
-			query: newGadget => ({
-				url: "/",
-				method: "POST",
-				body: newGadget,
-			}),
-		}),
-	}),
+  reducerPath: "gadgetApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_URL}/gadget`,
+    credentials: "include",
+  }),
+  tagTypes: ["Gadget"],
+  endpoints: (builder) => ({
+    createGadget: builder.mutation<
+      GadgetResponse,
+      {
+        id?: number;
+        name: string;
+        user_id: number;
+		image?: string;
+      }
+    >({
+      query: (newGadget) => ({
+        url: "/",
+        method: "POST",
+        body: newGadget,
+      }),
+    }),
+  }),
 });
 
 export const { useCreateGadgetMutation } = apiGadget;
