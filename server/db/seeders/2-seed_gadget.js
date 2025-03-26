c5045465377e656264ebd44c716b610e459b9558
ElgardEd <grigorykremlevsky@yandex.ru>
2025-03-26 22:10:09 +0300

    update images on gadgets

--- server/db/seeders/2-seed_gadget.js
+++ server/db/seeders/2-seed_gadget.js
@@ -7,9 +7,9 @@ module.exports = {
 			"Gadgets",
 			[
 				{
-					name: "Кроватка Happy Baby Mommy Love",
+					name: "Кроватка Happy Baby Mommy Lux",
 					price: "42 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742326474/xs2y5n4dsmlxi7wuzh5s.jpg",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014715/seed_gadgets/vjd1hshtzt4xpjhklgqr.jpg",
 					group: "Мебель",
 					description:
 						"Очень удобная и функциональная кроватка. Регулируемое дно позволяет использовать её долгое время, а качество материалов на высоте. Сборка простая, но требует внимательности. Отлично вписывается в интерьер.",
@@ -19,7 +19,7 @@ module.exports = {
 				{
 					name: "Стульчик для кормления Chicco Polly",
 					price: "12 500",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395332/tnscvvtimfgk9bjz4cvh.jpg",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014714/seed_gadgets/no2eqshcwuvevwabagbe.jpg",
 					group: "Мебель",
 					description:
 						"Практичный и компактный стульчик. Легко складывается, что удобно для хранения и транспортировки. Мягкие вставки съемные, их легко мыть. Ребенку удобно сидеть, а родителям — кормить. Рекомендую!",
@@ -29,7 +29,7 @@ module.exports = {
 				{
 					name: "Манеж Geuther Happy Play",
 					price: "18 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395456/ce1mc2asykppdridqyhz.jpg",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014714/seed_gadgets/jawmozalv8o8xfrm6xkr.jpg",
 					group: "Мебель",
 					description:
 						"Надежный и безопасный манеж. Просторный, с сетчатыми стенками, которые обеспечивают хороший обзор. Дно мягкое, но при этом прочное. Легко складывается и переносится. Идеально для активных малышей.",
@@ -39,7 +39,7 @@ module.exports = {
 				{
 					name: "Автокресло Britax Romer Kidfix",
 					price: "25 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395560/tairqxtcigc6nclqullq.jpg",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014715/seed_gadgets/xh6oyzbol3xuxvaepkaj.jpg",
 					group: "Транспорт",
 					description:
 						"Отличное автокресло для детей постарше. Удобное, с боковой защитой и регулируемой спинкой. Ребенок чувствует себя комфортно даже в долгих поездках. Качество материалов и сборки на высоте.",
@@ -49,7 +49,7 @@ module.exports = {
 				{
 					name: "Пеленальный столик IKEA Sundvik",
 					price: "8 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395649/kpcopz69ojw28e0hecgo.jpg",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014714/seed_gadgets/smthspctq1nxwvie0wui.jpg",
 					group: "Мебель",
 					description:
 						"Простой, но очень удобный столик. Вместительные полки позволяют хранить все необходимое под рукой. Легко собирается и устойчив. Отличное решение для организации пространства в детской.",
@@ -59,7 +59,7 @@ module.exports = {
 				{
 					name: "Велосипед Puky LR M",
 					price: "15 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395771/auw4c1ssnnj48nljouhk.webp",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014714/seed_gadgets/h5seleyrukwl9txs9qjm.jpg",
 					group: "Транспорт",
 					description:
 						"Качественный детский велосипед. Легкий, но прочный, с регулируемым сиденьем и рулем. Ребенок быстро научился кататься благодаря устойчивости и удобству. Отличный выбор для первых поездок.",
@@ -69,7 +69,7 @@ module.exports = {
 				{
 					name: "Игровой коврик Tiny Love Sunny Day",
 					price: "5 500",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395860/djo0oaoyhuukiud9v1ch.webp",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743014714/seed_gadgets/kf1pn4s9vk9crtx1ggqj.jpg",
 					group: "Игрушки",
 					description:
 						"Яркий и интересный коврик для малышей. Игрушки на дугах привлекают внимание, а мягкая поверхность удобна для игр. Легко складывается и чистится. Ребенок проводит на нем много времени с удовольствием.",
@@ -77,12 +77,12 @@ module.exports = {
 					updatedAt: new Date(),
 				},
 				{
-					name: "Рюкзак-переноска Ergobaby Omni 360",
-					price: "14 000",
-					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1742395966/gxaouydybjwfuuh6nrid.jpg",
+					name: "Детская коляска Cybex Priam IV 3 в 1 by Jeremy Scott Car",
+					price: "480 000",
+					image: "https://res.cloudinary.com/dlliagivo/image/upload/v1743015579/seed_gadgets/lbjauj1lfk9z22flqxgv.jpg",
 					group: "Аксессуары",
 					description:
-						"Удобный и эргономичный рюкзак-переноска. Подходит с рождения, регулируется под рост ребенка и родителя. Спина не устает даже после долгого ношения. Качество материалов и продуманность на высоте.",
+						"Стильная и функциональная коляска премиум-класса. Идеально управляется одной рукой, дизайн выделяет среди других моделей — выглядит дорого и необычно. Качество материалов и ходовая часть на высоте.",
 					createdAt: new Date(),
 					updatedAt: new Date(),
 				},
